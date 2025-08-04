0c62e5df020998c53e6b52b586920adb4102a79f
Djibril <najidjibril@gmail.com>
2025-08-04 19:27:16 +0200

    feature : rename varianle + fix vercel

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,12 +1,13 @@
 import { createAuthClient } from "better-auth/react"
 
-const baseURL = process.env.BASE_URL || "http://localhost:3000"
-if (!process.env.BASE_URL) {
+const nextPublicBaseURL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+
+if (!process.env.NEXT_PUBLIC_BASE_URL) {
   console.warn(
-    "Warning: BASE_URL environment variable is not set. Using default: http://localhost:3000"
+    "Warning: NEXT_PUBLIC_BASE_URL environment variable is not set. Using default: http://localhost:3000"
   )
 }
 
-const authClient = createAuthClient({ baseURL })
+const authClient = createAuthClient({ baseURL: nextPublicBaseURL })
 
 export const { signIn, signUp, useSession } = authClient
